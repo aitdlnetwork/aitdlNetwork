bee21377dc298ef02ec2bb465e6bb3cbb9ba0a7a
ganitsutram-ui <ganitsutram@gmail.com>
2026-03-24 02:05:19 +0530

    fix: switch to Next.js static export for Cloudflare Pages

---
+++ frontend/.wrangler/tmp/bundle-Ld1isO/middleware-insertion-facade.js
@@ -0,0 +1,18 @@
+/*
+AITDL Network © 2026 | Vikram Samvat 2083
+Author: Jawahar R Mallah
+Website: https://www.aitdl.com
+Contact: aitdlnetwork@outlook.com | jawahar.mallah@gmail.com
+*/
+
+				import worker, * as OTHER_EXPORTS from "D:\\IMP\\GitHub\\aitdlNetwork\\frontend\\node_modules\\wrangler\\templates\\pages-shim.ts";
+				import * as __MIDDLEWARE_0__ from "D:\\IMP\\GitHub\\aitdlNetwork\\frontend\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "D:\\IMP\\GitHub\\aitdlNetwork\\frontend\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "D:\\IMP\\GitHub\\aitdlNetwork\\frontend\\node_modules\\wrangler\\templates\\pages-shim.ts";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
