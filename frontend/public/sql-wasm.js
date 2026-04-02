cb31312bb8a22d56e4664ec8a8f5ed6a6a0c0776
ganitsutram-ui <ganitsutram@gmail.com>
2026-04-02 17:39:04 +0530

    feat: stabilize Micro-ERP engine with root-injection & manual WASM fetch

--- frontend/public/sql-wasm.js
+++ frontend/public/sql-wasm.js
@@ -1,3 +1,10 @@
+/*
+AITDL Network © 2026 | Vikram Samvat 2083
+Author: Jawahar R Mallah
+Website: https://www.aitdl.com
+Contact: aitdlnetwork@outlook.com | jawahar.mallah@gmail.com
+*/
+
 
 // We are modularizing this manually because the current modularize setting in Emscripten has some issues:
 // https://github.com/kripken/emscripten/issues/5820
--- frontend/src/lib/erp/DatabaseContext.tsx
+++ frontend/src/lib/erp/DatabaseContext.tsx
@@ -1,3 +1,10 @@
+/*
+AITDL Network © 2026 | Vikram Samvat 2083
+Author: Jawahar R Mallah
+Website: https://www.aitdl.com
+Contact: aitdlnetwork@outlook.com | jawahar.mallah@gmail.com
+*/
+
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import type { Database, SqlJsStatic } from 'sql.js';
 
